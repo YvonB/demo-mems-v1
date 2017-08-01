bab8410b6b610a0267b21e0e7769ee71dafd86de
YvonB <yvonbenahita@gmail.com>
2017-08-01 14:52:39 +0200

    ajout des 3 courbes

--- private/index.php
+++ private/index.php
@@ -17,10 +17,24 @@
 <body>
 
 	<!-- div permettant de visualiser la courbe -->
-	<div id="container" style="height: 400px; min-width: 310px">
-		
-	</div>
-
+    <div align="center">  
+        <h3>Gaz carbonique</h3>
+    	<div id="co2" style="height: 400px; min-width: 310px">
+    		
+    	</div>
+    </div> 
+    <div align="center">  
+        <h3>Monoxyde de Carbone</h3>
+        <div id="co" style="height: 400px; min-width: 310px">
+            
+        </div>
+    </div>
+    <div align="center">  
+        <h3>Amoniaque</h3>
+        <div id="nh3" style="height: 400px; min-width: 310px">
+            
+        </div>
+    </div> 
     <!-- pour récupérer les valeurs dans la BD -->
     <?php
         // Inclusion
@@ -42,7 +56,7 @@
 		{
 
             // create the chart
-            Highcharts.stockChart('container', {
+            Highcharts.stockChart('co2', {
 
 
                 title: {
@@ -101,6 +115,168 @@
 });
 
 	</script>
+<!-- ***************************************** -->
+<!-- pour récupérer les valeurs dans la BD -->
+    <?php
+        // Inclusion
+        require_once('../vendor/autoload.php');
+
+        // Chercher les dernières valeurs insérées
+        $obj_repo = new \GDS\Demo\Repository();
+        $arr_posts = $obj_repo->getRecentPosts(); 
+
+        foreach ($arr_posts as $obj_post)
+        {
+            $data[] = $obj_post->co;
+        }
+    ?>
+    
+    <!-- le script de la courbe lui même -->
+    <script type="text/javascript">
+        $.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=new-intraday.json&callback=?', function () 
+        {
+
+            // create the chart
+            Highcharts.stockChart('co', {
+
+
+                title: {
+                    text: 'Valeurs de Monoxyde de carbone par minute'
+                },
+
+                subtitle: {
+                    text: 'En partie par million (ppm)'
+                },
+
+                xAxis: {
+                    gapGridLineWidth: 0
+                },
+
+                rangeSelector: {
+                    buttons: [{
+                        type: 'hour',
+                        count: 1,
+                        text: '1h'
+                    }, {
+                        type: 'day',
+                        count: 1,
+                        text: '1j'
+                    }, {
+                        type: 'all',
+                        count: 1,
+                        text: 'Tous'
+                    }],
+                    selected: 1,
+                    inputEnabled: false
+                },
+
+                series: [{
+                    name: 'CO',
+                    type: 'area',
+                    data: [<?php echo join($data, ',') ?>],
+                    gapSize: 5,
+                    tooltip: {
+                        valueDecimals: 2
+                    },
+                    fillColor: {
+                        linearGradient: {
+                            x1: 0,
+                            y1: 0,
+                            x2: 0,
+                            y2: 1
+                        },
+                        stops: [
+                            [0, Highcharts.getOptions().colors[0]],
+                            [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                        ]
+                    },
+                    threshold: null
+                }]
+            });
+});
+
+    </script>
+<!-- ***************************************** -->
+<!-- pour récupérer les valeurs dans la BD -->
+    <?php
+        // Inclusion
+        require_once('../vendor/autoload.php');
+
+        // Chercher les dernières valeurs insérées
+        $obj_repo = new \GDS\Demo\Repository();
+        $arr_posts = $obj_repo->getRecentPosts(); 
+
+        foreach ($arr_posts as $obj_post)
+        {
+            $data[] = $obj_post->nh3;
+        }
+    ?>
+    
+    <!-- le script de la courbe lui même -->
+    <script type="text/javascript">
+        $.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=new-intraday.json&callback=?', function () 
+        {
+
+            // create the chart
+            Highcharts.stockChart('nh3', {
+
+
+                title: {
+                    text: 'Valeurs d\'Amoniaque par minute'
+                },
+
+                subtitle: {
+                    text: 'En partie par million (ppm)'
+                },
+
+                xAxis: {
+                    gapGridLineWidth: 0
+                },
+
+                rangeSelector: {
+                    buttons: [{
+                        type: 'hour',
+                        count: 1,
+                        text: '1h'
+                    }, {
+                        type: 'day',
+                        count: 1,
+                        text: '1j'
+                    }, {
+                        type: 'all',
+                        count: 1,
+                        text: 'Tous'
+                    }],
+                    selected: 1,
+                    inputEnabled: false
+                },
+
+                series: [{
+                    name: 'NH3',
+                    type: 'area',
+                    data: [<?php echo join($data, ',') ?>],
+                    gapSize: 5,
+                    tooltip: {
+                        valueDecimals: 2
+                    },
+                    fillColor: {
+                        linearGradient: {
+                            x1: 0,
+                            y1: 0,
+                            x2: 0,
+                            y2: 1
+                        },
+                        stops: [
+                            [0, Highcharts.getOptions().colors[0]],
+                            [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                        ]
+                    },
+                    threshold: null
+                }]
+            });
+});
+
+    </script>
 <!-- ***************************************** -->
 
 </body>
