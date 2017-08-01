a78037162c4343f1e6b2f32223926d913baf9562
YvonB <yvonbenahita@gmail.com>
2017-08-01 08:03:37 +0200

    tout fonctionne

--- private/index.php
+++ private/index.php
@@ -5,8 +5,9 @@
 	<title>Détéction de Pollution</title>
 
 	<!-- script pour la courbe -->
-	<script src="https://code.highcharts.com/highcharts.js"></script>
-	<script src="https://code.highcharts.com/modules/exporting.js"></script>
+	<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+	<script src="https://code.highcharts.com/stock/highstock.js"></script>
+	<script src="https://code.highcharts.com/stock/modules/exporting.js"></script>
 	<!-- ********************** -->
 	<style type="text/css">
 		.highcharts-credits
@@ -16,73 +17,74 @@
 <body>
 
 	<!-- div permettant de visualiser la courbe -->
-	<div id="container" style="min-width: 310px; height: 400px; margin: 0 auto">
+	<div id="container" style="height: 400px; min-width: 250px">
 		
 	</div>
 	
 	<!-- le script de la courbe lui même -->
 	<script type="text/javascript">
-		Highcharts.chart('container', {
-    chart: {
-        type: 'column'
-    },
-    title: {
-        text: 'Pollution detection'
-    },
-    subtitle: {
-        text: 'Source: https://github.com/YvonB/demo-mems-v1'
-    },
-    xAxis: {
-        categories: [
-            'Jan',
-            'Feb',
-            'Mar',
-            'Apr',
-            'May',
-            'Jun',
-            'Jul',
-            'Aug',
-            'Sep',
-            'Oct',
-            'Nov',
-            'Dec'
-        ],
-        crosshair: true
-    },
-    yAxis: {
-        min: 0,
+		$.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=new-intraday.json&callback=?', function (data) 
+		{
+
+    // create the chart
+    Highcharts.stockChart('container', {
+
+
         title: {
-            text: 'Values (ppm)'
-        }
-    },
-    tooltip: {
-        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-        pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-            '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
-        footerFormat: '</table>',
-        shared: true,
-        useHTML: true
-    },
-    plotOptions: {
-        column: {
-            pointPadding: 0.2,
-            borderWidth: 0
-        }
-    },
-    series: [{
-        name: 'Gaz carbonique',
-        data: [83.6, 78.8, 98.5, 93.4, 106.0, 84.5, 105.0, 104.3, 91.2, 83.5, 106.6, 92.3]
+            text: 'AAPL stock price by minute'
+        },
 
-    }, {
-        name: 'Monoxyde de carbone',
-        data: [48.9, 38.8, 39.3, 41.4, 47.0, 48.3, 59.0, 59.6, 52.4, 65.2, 59.3, 51.2]
+        subtitle: {
+            text: 'Using ordinal X axis'
+        },
 
-    }, {
-        name: 'Amoniaque',
-        data: [42.4, 33.2, 34.5, 39.7, 52.6, 75.5, 57.4, 60.4, 47.6, 39.1, 46.8, 51.1]
+        xAxis: {
+            gapGridLineWidth: 0
+        },
 
-    }]
+        rangeSelector: {
+            buttons: [{
+                type: 'hour',
+                count: 1,
+                text: '1h'
+            }, {
+                type: 'day',
+                count: 1,
+                text: '1D'
+            }, {
+                type: 'all',
+                count: 1,
+                text: 'All'
+            }],
+            selected: 1,
+            inputEnabled: false
+        },
+
+        series: [{
+            name: 'AAPL',
+            type: 'area',
+            data: data,
+            gapSize: 5,
+            tooltip: {
+                valueDecimals: 2
+            },
+            fillColor: {
+                linearGradient: {
+                    x1: 0,
+                    y1: 0,
+                    x2: 0,
+                    y2: 1
+                },
+                stops: [
+                    [0, Highcharts.getOptions().colors[0]],
+                    [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                ]
+            },
+            threshold: null
+        }]
+    });
 });
+
 	</script>
 <!-- ***************************************** -->
 
