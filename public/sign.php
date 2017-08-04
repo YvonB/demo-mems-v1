7472b917955706f8b99fedde73b52a63219cbe74
YvonB <yvonbenahita@gmail.com>
2017-08-04 06:39:03 +0200

    au lieu de afficher le membre nouvellement inscri, j'ai fais une redirection

--- public/sign.php
+++ public/sign.php
@@ -38,17 +38,17 @@ if (!empty($_POST['nom']) AND !empty($_POST['mail']) AND !empty($_POST['mdp']) A
 
 			$obj_member->createMember($nom, $mail, $mdp, $_SERVER['REMOTE_ADDR']); // afin qu'on puisse appéler sa methode pour créer un membre.
 
-			// header("Location: /sign.php?news=nouvel utlisateur crée");
+			header("Location: /sign.php?news=users");
 
 			// chercher les membres
-			$arr_posts = $obj_member->getRecentMember();
-			// Afficher les
-			echo "<div align='center'>";
-				foreach ($arr_posts as $obj_post ) 
-				{
-					echo '<h1> Bienvenue, ', $obj_post->nom, '</h1>'; echo '<br>';
-				}
-			echo "</div>";
+			// $arr_posts = $obj_member->getRecentMember();
+			// // Afficher les
+			// echo "<div align='center'>";
+			// 	foreach ($arr_posts as $obj_post ) 
+			// 	{
+			// 		echo '<h1> Bienvenue, ', $obj_post->nom, '</h1>'; echo '<br>';
+			// 	}
+			// echo "</div>";
 		}
 		else
 		{
