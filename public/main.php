8a5a5ec12452619790eac853b3efbd16e7976aa1
YvonB <yvonbenahita@gmail.com>
2017-08-08 15:03:34 +0200

    ca marche mais ya quelques trucs à regler

--- public/main.php
+++ public/main.php
@@ -144,52 +144,54 @@ require_once('../vendor/autoload.php');
                     // Chercher les dernières valeurs insérées
                     $arr_posts = $obj_repo->getLatestPost();
 
-                    echo "<pre>";
-                    echo print_r($arr_posts);
-                    echo "</pre>";
-
+                    foreach ($arr_posts as $obj_post) 
+                    {
+                        
                     ?>
                     <!-- Fin requêtes -->
 
-                    <script type="text/javascript">
-                        google.charts.load('current', {'packages':['gauge']});
-                        google.charts.setOnLoadCallback(drawChart);
+                        <script type="text/javascript">
+                            google.charts.load('current', {'packages':['gauge']});
+                            google.charts.setOnLoadCallback(drawChart);
 
-                        function drawChart() {
+                            function drawChart() {
 
-                        var data = google.visualization.arrayToDataTable([
-                          ['Label', 'Value'],
-                          ['CO2', 80],
-                          ['CO', 55],
-                          ['NH3', 68]
-                        ]);
+                            // Les compteur commence à 0.
+                            var data = google.visualization.arrayToDataTable([
+                              ['Label', 'Value'],
+                              ['CO2', 0],
+                              ['CO', 0],
+                              ['NH3', 0]
+                            ]);
 
-                        var options = {
-                          width: 400, height: 120,
-                          redFrom: 90, redTo: 100,
-                          yellowFrom:75, yellowTo: 90,
-                          minorTicks: 5
-                        };
+                            var options = {
+                              width: 400, height: 120,
+                              redFrom: 90, redTo: 100,
+                              yellowFrom:75, yellowTo: 90,
+                              minorTicks: 5
+                            };
 
-                        var chart = new google.visualization.Gauge(document.getElementById('chart_div'));
+                            var chart = new google.visualization.Gauge(document.getElementById('chart_div'));
 
-                        chart.draw(data, options);
+                            chart.draw(data, options);
 
-                        setInterval(function() {
-                          data.setValue(0, 1, 40 + Math.round(60 * Math.random()));
-                          chart.draw(data, options);
-                        }, 13000);
-                        setInterval(function() {
-                          data.setValue(1, 1, 40 + Math.round(60 * Math.random()));
-                          chart.draw(data, options);
-                        }, 5000);
-                        setInterval(function() {
-                          data.setValue(2, 1, 60 + Math.round(20 * Math.random()));
-                          chart.draw(data, options);
-                        }, 26000);
-                      }
-                    </script>
-
+                            setInterval(function() {
+                              data.setValue(0, 1, 0 + <?php echo $obj_post->co2; ?>);
+                              chart.draw(data, options);
+                            }, 13000);
+                            setInterval(function() {
+                              data.setValue(1, 1, 0 + <?php echo $obj_post->co; ?>);
+                              chart.draw(data, options);
+                            }, 5000);
+                            setInterval(function() {
+                              data.setValue(2, 1, 0 + <?php echo $obj_post->nh3; ?>);
+                              chart.draw(data, options);
+                            }, 26000);
+                          }
+                        </script>
+                    <?php    
+                    }
+                    ?>
                 </div>
                 </div>
             </div>
