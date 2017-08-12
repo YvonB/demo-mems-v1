2db53db784713d5c005eb0b79b92a2353e054068
YvonB <yvonbenahita@gmail.com>
2017-08-12 19:54:20 +0200

    ident slide du home

--- private/home.php
+++ private/home.php
@@ -153,68 +153,82 @@
                       <a id="next" href="#">&#8811;</a>
                     </div>
                     </div>
-                    <!-- 2) CSS -->
 
+                    <!-- 2) CSS : slide.css-->
 
                     <!-- 3) JS -->
                     <script type="text/javascript">
 
-                      var responsiveSlider = function() {
+                      var responsiveSlider = function() 
+                      {
+                          var slider = document.getElementById("slider");
+                          var sliderWidth = slider.offsetWidth;
+                          var slideList = document.getElementById("slideWrap");
+                          var count = 1;
+                          var items = slideList.querySelectorAll("li").length;
+                          var prev = document.getElementById("prev");
+                          var next = document.getElementById("next");
 
-                      var slider = document.getElementById("slider");
-                      var sliderWidth = slider.offsetWidth;
-                      var slideList = document.getElementById("slideWrap");
-                      var count = 1;
-                      var items = slideList.querySelectorAll("li").length;
-                      var prev = document.getElementById("prev");
-                      var next = document.getElementById("next");
+                          window.addEventListener('resize', function() 
+                                                              {
+                                                                sliderWidth = slider.offsetWidth;
+                                                              }
+                                                  );
 
-                      window.addEventListener('resize', function() {
-                        sliderWidth = slider.offsetWidth;
-                      });
+                          var prevSlide = function() 
+                                              {
+                                                if(count > 1) 
+                                                    {
+                                                      count = count - 2;
+                                                      slideList.style.left = "-" + count * sliderWidth + "px";
+                                                      count++;
+                                                    }
+                                                else if(count = 1) 
+                                                    {
+                                                      count = items - 1;
+                                                      slideList.style.left = "-" + count * sliderWidth + "px";
+                                                      count++;
+                                                    }
+                                              };
 
-                      var prevSlide = function() {
-                        if(count > 1) {
-                          count = count - 2;
-                          slideList.style.left = "-" + count * sliderWidth + "px";
-                          count++;
-                        }
-                        else if(count = 1) {
-                          count = items - 1;
-                          slideList.style.left = "-" + count * sliderWidth + "px";
-                          count++;
-                        }
-                      };
+                          var nextSlide = function() 
+                                              {
+                                                if(count < items) 
+                                                    {
+                                                      slideList.style.left = "-" + count * sliderWidth + "px";
+                                                      count++;
+                                                    }
+                                                else if(count = items) 
+                                                    {
+                                                      slideList.style.left = "0px";
+                                                      count = 1;
+                                                    }
+                                              };
+                          
+                          next.addEventListener("click", function() 
+                                                              {
+                                                                nextSlide();
+                                                              }
+                                               );
 
-                      var nextSlide = function() {
-                        if(count < items) {
-                          slideList.style.left = "-" + count * sliderWidth + "px";
-                          count++;
-                        }
-                        else if(count = items) {
-                          slideList.style.left = "0px";
-                          count = 1;
-                        }
-                      };
-                      
-                      next.addEventListener("click", function() {
-                        nextSlide();
-                      });
-
-                      prev.addEventListener("click", function() {
-                        prevSlide();
-                      });
-                      
-                      setInterval(function() {
-                        nextSlide()
-                      }, 8000);
+                          prev.addEventListener("click", function() 
+                                                              {
+                                                                prevSlide();
+                                                              }
+                                                );
+                          
+                          setInterval(function() 
+                                          {
+                                            nextSlide()
+                                          }, 8000
+                                     );
 
                     };
 
-                    window.onload = function() {
+                    window.onload = function()
+                     {
                       responsiveSlider();  
-                    }
-
+                     }
                     </script>
 
 
