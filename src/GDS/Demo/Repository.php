be1bbab9ece7ec6fdd30134447eb4f4136d3c8ef
YvonB <yvonbenahita@gmail.com>
2017-08-12 17:36:34 +0200

    format/comment Repository()

--- src/GDS/Demo/Repository.php
+++ src/GDS/Demo/Repository.php
@@ -1,9 +1,11 @@
 <?php
+
 /**
  * Dépôt pour GDS
  *
  * @author Yvon Benahita
  */
+
 namespace GDS\Demo;
 use GDS\Schema;
 use GDS\Store;
@@ -37,46 +39,57 @@ class Repository
     }
 
     /**
-     * Prendre les valeurs 10 insérées les plus récentes.(pour l'affichage brute) 
+     * Pour prendre les 10 valeurs insérées les plus récentes.(pour l'affichage brute) 
      *
      * @return array
      */
     public function getRecentPosts()
     {
         $arr_posts = $this->getCache()->get('recent');
-        if(is_array($arr_posts)) {
-            return $arr_posts;
-        } else {
-            return $this->updateCache();
-        }
+
+        if(is_array($arr_posts)) 
+            {
+                return $arr_posts;
+            } 
+        else 
+            {
+                return $this->updateCache();
+            }
     }
 
     /**
-     * Pour prendre les valeurs insérées les plus récentes. (pour le gauge)
+     * Pour prendre TOUS les valeurs insérées les plus récentes.(pour le gauge)
      *
      * @return array
      */
     public function getAllRecentPost()
     {
         $arr_posts = $this->getCache()->get('recent');
-        if(is_array($arr_posts)) {
-            return $arr_posts;
-        } else {
-            return $this->updateAllCache();
-        }
+
+        if(is_array($arr_posts)) 
+            {
+                return $arr_posts;
+            } 
+        else 
+            {
+                return $this->updateAllCache();
+            }
     }
 
 
     /**
-     * Mettre à jour le cache de Datastore (pour l'affichage brute des 1à dernières valeurs)
+     * Mettre à jour le cache de Datastore (pour l'affichage brute des 10 dernières valeurs)
      *
      * @return array
      */
     private function updateCache()
     {
         $obj_store = $this->getStore();
+
         $arr_posts = $obj_store->query("SELECT * FROM Gas ORDER BY posted DESC")->fetchPage(POST_LIMIT);
+
         $this->getCache()->set('recent', $arr_posts);
+
         return $arr_posts;
     }
 
@@ -88,8 +101,11 @@ class Repository
     private function updateAllCache()
     {
         $obj_store = $this->getStore();
+
         $arr_posts = $obj_store->query("SELECT * FROM Gas")->fetchAll();
+
         $this->getCache()->set('recent',$arr_posts);
+
         return $arr_posts;
     }
 
@@ -103,6 +119,7 @@ class Repository
     public function createPost($str_co2, $str_co, $str_nh3)
     {
         $obj_store = $this->getStore();
+
         $obj_store->upsert($obj_store->createEntity([
             'posted' => date('Y-m-d H:i:s'),
             'co2' => $str_co2,
@@ -115,23 +132,24 @@ class Repository
     }
 
     /**
-     * Configuration et retourner un magasin
+     * Configuration et retourner un magasin (Store)
      *
      * @return Store
      */
     private function getStore()
     {
         if(NULL === $this->obj_store) 
-        {
-            $this->obj_store = new Store($this->makeSchema());
-        }
+            {
+                $this->obj_store = new Store($this->makeSchema());
+            }
+
         return $this->obj_store;
     }
 
     /**
      * Créez un schéma pour les entrées (on peut dire Table)
      *
-     * 'posted' est l'heure de la date d'entrée des valeur 
+     * 'posted' est l'heure de la date d'entrée des valeurs 
      *
      * @return Schema
      */
